66ae9b021ac4f959b7a890f3db7c195050278dfd
Chua Chloe <chloechua18@gmail.com>
2026-01-21 01:46:43 +0800

    Complete Level-0

---
+++ src/main/java/Sqonky.java
@@ -0,0 +1,11 @@
+public class Sqonky {
+    public static void main(String[] args) {
+        String hello = " ____________________________________________________________\n"
+                + " Hello! I'm Sqonky\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+        String goodbye = " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(hello + goodbye);
+    }
+}

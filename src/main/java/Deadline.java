b1be326611df4ecb8373fa296c86c1d81fede559
Chua Chloe <chloechua18@gmail.com>
2026-01-29 01:07:16 +0800

    Level-8: Recognise dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that must be completed by a specified deadline.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
+    }
+
+    public LocalDateTime getBy() {
+        return by;
     }
 
     @Override
@@ -15,6 +22,7 @@ public class Deadline extends Task {
     }
 
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                by.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a")));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,31 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that occurs over a specific time period.
  */
 public class Event extends Task{
-    protected String from, to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
     @Override
     public String toSaveFormat() {
-        return "E | " + super.toSaveFormat() + " | " + from + "-" + to;
+        return "E | " + super.toSaveFormat() + " | " + from + " to " + to;
     }
 
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), from, to);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
+        return String.format("[E]%s (from: %s to: %s)", super.toString(),
+                from.format(fmt), to.format(fmt));
     }
 }
